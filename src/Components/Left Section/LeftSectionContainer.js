eece6232561547bac91647a428ef44d313ada5ee
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-12-02 06:22:28 +0100

    add back button

--- src/Components/Left Section/LeftSectionContainer.js
+++ src/Components/Left Section/LeftSectionContainer.js
@@ -8,22 +8,24 @@ const LeftSection = (props) => {
   // console.log(props.weatherData);
 
   return (
-    <section className="Left-section">
-      <div className="Left-Section__top">
-        <Title />
-        <Icon />
-        <div className="Left-section__flex">
-          <Temperature temperature={props.weatherData.main.temp} />
-          <TemperatureDescription
-            temperatureDescription={props.weatherData.weather[0].description}
-          />
+    <>
+      <section className="Left-section">
+        <div className="Left-Section__top">
+          <Title />
+          <Icon />
+          <div className="Left-section__flex">
+            <Temperature temperature={props.weatherData.main.temp} />
+            <TemperatureDescription
+              temperatureDescription={props.weatherData.weather[0].description}
+            />
+          </div>
         </div>
-      </div>
-      <AdditionalInformation
-        humidity={props.weatherData.main.humidity}
-        windSpeed={props.weatherData.wind.speed}
-      />
-    </section>
+        <AdditionalInformation
+          humidity={props.weatherData.main.humidity}
+          windSpeed={props.weatherData.wind.speed}
+        />
+      </section>
+    </>
   );
 };
 export default LeftSection;
--- src/Components/Left Section/Title.js
+++ src/Components/Left Section/Title.js
@@ -1,6 +1,51 @@
 import "./Title.css";
+const BackButton = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+      />
+    </svg>
+  );
+};
 
 const Title = () => {
-  return <h1 className="Title">Weather</h1>;
+  return (
+    <>
+      <div className="Title">
+        <span className="BackButton">
+          {
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+              />
+            </svg>
+          }
+        </span>
+
+        <span>
+          <h1 className="Title"> Weather</h1>
+        </span>
+      </div>
+    </>
+  );
 };
 export default Title;
